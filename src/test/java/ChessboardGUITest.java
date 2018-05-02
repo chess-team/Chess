ef47027170cdb165bef9dddf3f9fd6017b1901fc
edyta.garbarz <z1143296@student.tcs.uj.edu.pl>
2018-05-02 12:26:33 +0200

    Simple chessboard GUI

---
+++ src/test/java/ChessboardGUITest.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ChessboardGUITest extends JFrame{
+    public ChessboardGUITest() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        ChessboardGUI chess= new ChessboardGUI(new GridLayout(10,10));
+        chess.setColorType2();
+        chess.setIconsType1();
+        chess.initChessboard();
+        createLayout(chess);
+        JPanel chesscontainer=new JPanel();
+        chesscontainer.add(chess);
+        createLayout(chesscontainer);
+        this.add(chesscontainer);
+        setTitle("Chessboard");
+        setSize(800, 800);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+    private void createLayout(JComponent... arg) {
+
+        Container pane = getContentPane();
+        GroupLayout gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            ChessboardGUITest ex = new ChessboardGUITest();
+            ex.setVisible(true);
+        });
+    }
+}
