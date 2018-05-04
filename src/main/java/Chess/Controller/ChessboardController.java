56a8a1b192b1582ca23493127577386e6957f398
Krzysztof Pioro <krzysztof.pioro1998@gmail.com>
2018-05-04 17:12:15 +0200

    Adding promotion menu

--- src/main/java/Chess/Controller/ChessboardController.java
+++ src/main/java/Chess/Controller/ChessboardController.java
@@ -1,8 +1,7 @@
 package Chess.Controller;
 
 import Chess.Model.ChessColour;
-import Chess.Model.ChessPieces.ChessPiece;
-import Chess.Model.ChessPieces.EmptySquare;
+import Chess.Model.ChessPieces.*;
 import Chess.Model.Chessboard;
 import Chess.Model.Moves.Move;
 import Chess.Model.Position;
@@ -19,8 +18,15 @@ public class ChessboardController {
     private MainPanelView mainPanelView;
     private ChessboardView chessboardView;
     private Position from;
+    private String promote;
+
+    public void setPromote(String promote) {
+        this.promote = promote;
+    }
+
 
     public ChessboardController(MainFrameView mainFrameView){
+        promote = "D";
         this.mainFrameView = mainFrameView;
         this.mainPanelView = mainFrameView.getMainPanelView();
         this.chessboardView = this.mainPanelView.getChessboardView();
@@ -34,31 +40,68 @@ public class ChessboardController {
         ActionListener buttonListener = actionEvent -> {
             String [] s = actionEvent.getActionCommand().split(" ",2);
             Position position = new Position(Integer.valueOf(s[0]),Integer.valueOf(s[1]));
+            ChessPiece figure;
 
-            if( from == null ){
-                ChessPiece figure = StateOfGame.chessboard.getChessPieceOnPosition(position);
-                if( figure.getClass() != EmptySquare.class && isColorOfMovedPieceCorrect(figure.getChessColour()) ){
-                    //System.out.println("TAKING PIECE");
-                    takePiece(position);
-
-                }//else System.out.println("INVALID PIECE");
-            }else {
-                Move move = new Move(from,position);
-                if( StateOfGame.variant.validateMove(move) ){
-                    //System.out.println("Moving piece");
-                    movePiece(move);
-                    checkState();
-
-                }else{
-                    //System.out.println("INVALID MOVE");
-                    takePieceUndo();
+            if( promote == "D"){  //no promotion
+                if( from == null ){ // TAKING PIECE
+                    figure = StateOfGame.chessboard.getChessPieceOnPosition(position);
+                    if( figure.getClass() != EmptySquare.class && isColorOfMovedPieceCorrect(figure.getChessColour()) ){
+                        takePiece(position);
+                    }
+                }else {
+                    Move move = new Move(from,position);
+                    if( StateOfGame.variant.validateMove(move) ){
+                        movePiece(move);
+                        checkState();
+                    }else{
+                        takePieceUndo();
+                    }
                 }
+            }else {
+                if( from == null ){
+                    figure = StateOfGame.chessboard.getChessPieceOnPosition(position);
+                    if( figure.getClass() == Pawn.class && isColorOfMovedPieceCorrect(figure.getChessColour())){
+                        takePieceForPromotion(position);
+                    }
+                }else {
+                    ChessPiece newChessPiece;
+                    figure = StateOfGame.chessboard.getChessPieceOnPosition(from);
+                    switch( promote ){
+                        case "Q":
+                            newChessPiece = new Queen(figure.getChessColour(),position);
+                            break;
+                        case "R":
+                            newChessPiece = new Rook(figure.getChessColour(),position);
+                            break;
+                        case "K":
+                            newChessPiece = new Knight(figure.getChessColour(),position);
+                            break;
+                        case "B":
+                            newChessPiece = new Bishop(figure.getChessColour(),position);
+                            break;
+                        default:
+                            newChessPiece = new Pawn(figure.getChessColour(),position);
+                    }
+                    Move move = new Move(from,position,newChessPiece);
+                    if( StateOfGame.variant.validateMove(move) ){
+                        movePieceWithPromotion(move);
+                        checkState();
+                    }else{
+                        takePieceForPromotionUndo();
+                    }
 
+                }
             }
+
+
+
         };
         chessboardView.setActionListener(buttonListener);
 
     }
+
+
+
     private boolean isColorOfMovedPieceCorrect (ChessColour colorOfMovedPiece){
         switch (StateOfGame.getStateOfGameplay()){
             case WHITE_MOVE:
@@ -83,6 +126,20 @@ public class ChessboardController {
                 break;
         }
     }
+    private void takePieceForPromotion(Position position) {
+        chessboardView.highlightPosition(position);
+        from = position;
+    }
+    private void movePieceWithPromotion(Move move){
+        chessboardView.highlightPositionUndo(from);
+        StateOfGame.variant.changeState(move);
+        mainFrameView.updateView();
+        from = null;
+    }
+    private void takePieceForPromotionUndo(){
+        chessboardView.highlightPositionUndo(from);
+        from = null;
+    }
 
     private void takePiece(Position position){
         chessboardView.highlightPosition(position);
@@ -101,4 +158,8 @@ public class ChessboardController {
         chessboardView.highlightPossiblePositionsUndo(from);
         from = null;
     }
+
+
+
+
 }
